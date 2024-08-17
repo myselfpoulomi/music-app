0f8165045a84b6e1821e2aae853dfb0d29f549d7
Subhajit Ghosh <subhajitghosh343@gmail.com>
2024-08-17 23:24:52 +0530

    connecting mongodb

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express()
-const port = process.env.PORT || 5101;
+const PORT = process.env.PORT || 5101;
+const MONGO_URL = process.env.MONGO_URL
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+mongoose.connect(MONGO_URL).then(() => {
+    app.listen(PORT, () => {
+        console.log("Connected to Database");
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}).catch((err) => {
+    console.error("Database connection error:", err);
 });
