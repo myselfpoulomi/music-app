d7072f1a6c5a982f9035e55597bce10e00754c56
myselfpoulomi <routhpoulomi@gmail.com>
2024-08-22 01:47:05 +0530

    update artist

--- client/src/components/admin/Artist/UpdateArtistcard.jsx
+++ client/src/components/admin/Artist/UpdateArtistcard.jsx
@@ -1,38 +1,83 @@
-import React from 'react'
-import './UpdateArtistCard.css'
-import axios from 'axios';
+import React from "react";
+import "./UpdateArtistCard.css";
+import axios from "axios";
+import { useState } from "react";
 
-function UpdateArtistcard({name,image,id}) {
+function UpdateArtistcard({ name, image, id }) {
   const handleDeleteArtist = async () => {
-    // console.log(name);
-    // console.log(id);
     try {
-      const Response = await axios.delete (`http://localhost:5100/admin/deleteartist/${id}`);
+      const Response = await axios.delete(
+        `http://localhost:5100/admin/deleteartist/${id}`
+      );
       console.log(Response);
-      if (Response.status==200) {
+      if (Response.status == 200) {
         alert("Artist Deleted Sucessfully");
       }
-      
-    }catch(error) {
+    } catch (error) {
       console.log(error);
-      
     }
+  };
 
-    
-  }
+  const [artistname, setname] = useState("");
+  const [file, setfile] = useState(null);
+  const handleName = (e) => {
+    const artistname = e.target.value;
+    setname(artistname);
+  };
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file.type.startsWith("image/")) setfile(file);
+    else alert("inert image only");
+  };
+
+  const handleUpdateArtist = async () => {
+    if (artistname == "" || file == null) {
+      alert("put name and choose file");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("name", artistname);
+    formData.append("file", file);
+    try {
+      const response = await axios.post(
+        "http://localhost:5100/admin/addartist",
+        formData
+      );
+      if (response.status == 200) {
+        alert("Artist Added");
+      }
+      console.log(response);
+    } catch (error) {
+      console.log("error while adding artist", error);
+    }
+  };
 
   return (
-    <div className='UpdateCardContainer'>
-        <div className="imgofartist">
-            <img src={image} alt="" />
-            <h3>{name}</h3>
+    <div className="UpdateCardContainer">
+      <div className="profileofartist">
+        <img src={image} alt="" />
+        <h3>{name}</h3>
+        <div className="inputsafterupdate">
+          <input
+            className="updateArtistName"
+            type="text"
+            placeholder="Enter Artist Name"
+            onChange={handleName}
+            value={artistname}
+          />
+          <input
+            className="updateArtistImg"
+            type="file"
+            onChange={handleFileChange}
+          />
         </div>
-        <div className="updatebtns">
-            <button>Update</button>
-            <button onClick={handleDeleteArtist}>Delete</button>
-        </div>
+      </div>
+      <div className="updatebtns">
+        <button onClick={handleUpdateArtist}>Update</button>
+        <button onClick={handleDeleteArtist}>Delete</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UpdateArtistcard
+export default UpdateArtistcard;
