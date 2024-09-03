942325524753e393d7ab0b7c19bba65f28203b6f
myselfpoulomi <routhpoulomi@gmail.com>
2024-09-04 00:54:28 +0530

    new css and new data

--- client/src/components/Songs/SongRow.jsx
+++ client/src/components/Songs/SongRow.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 import axios from "axios";
 
 function SongRow() {
@@ -15,21 +16,27 @@ function SongRow() {
     }
     getAllSongs();
   }, []);
-      
+
   return (
-    <div className="  p-[1rem] px-[3rem]">
-      <h2 className="text-white text-[35px] mb-9 ">Songs</h2>
-      <div className="flex gap-[2rem]">
+    <div className=" w-[100%] p-[20px]">
+      <div className="flex text-white justify-between items-center mb-[40px]">
+      <h2 className="text-white text-4xl pl-[30px]">Songs</h2>
+      <p className="flex gap-[20px] pr-[20px] text-lg">
+        <FaArrowLeft />
+        <FaArrowRight />
+      </p>
+      </div>
+      <div className="flex gap-[0.1rem] pl-[40px] mt-[20px] h-[78%]">
         {songlist.map((items, index) => {
           return (
-            <div>
+            <div className=" h-[270px] w-[280px] hover:bg-[rgba(31,18,18,0.252)] rounded-[10px] flex flex-col items-center justify-center  transition-all duration-700 ease-in-out hover:shadow-[rgba(0,0,0,0.3)]">
               <img
-                className="h-[210px] w-[210px] object-cover rounded-[7px] mb-8 "
+                className="h-[200px] w-[200px] object-cover rounded-[7px] mb-3 transform transition-transform duration-300 ease-in-out hover:scale-105"
                 src={items.image}
                 alt=""
               />
               <p className="text-white text-[20px] text-center mt-[5px]">
-                {items.title} 
+                {items.title}
               </p>
             </div>
           );
