a3ad93e5c648df3306bc5fc79f9ffb7f38194e99
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-07 01:10:59 +0530

    scroll in artist list

--- client/src/components/ArtistRow/ArtistRow.jsx
+++ client/src/components/ArtistRow/ArtistRow.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./ArtistRow.css";
 import Artistcard from "./ArtistCard/Artistcard";
 import { FaArrowLeft } from "react-icons/fa";
@@ -7,9 +7,9 @@ import axios from "axios";
 
 function ArtistRow() {
   const [artistList, setartistList] = useState([]);
+  const artistContainerRef = useRef(null);
   useEffect(() => {
     async function getallArtist() {
-      
       try {
         const Response = await axios.get("http://localhost:5100/getallartists");
         setartistList(Response.data.artistList);
@@ -20,18 +20,37 @@ function ArtistRow() {
     getallArtist();
   }, []);
 
+  function slideRight() {
+    if (artistContainerRef.current) {
+      artistContainerRef.current.scrollTo({
+        left: artistContainerRef.current.scrollLeft + 300,
+        behavior: "smooth"
+      });
+    }
+  }
+  function slideLeft() {
+    if (artistContainerRef.current) {
+      artistContainerRef.current.scrollTo({
+        left: artistContainerRef.current.scrollLeft - 300,
+        behavior: "smooth"
+      });
+    }
+  }
+
   return (
     <div className="ArtistRow">
       <div className="headingForArtist">
         <h2>Popular Artist</h2>
         <p className="arrows">
-          <FaArrowLeft />
-          <FaArrowRight />
+          <FaArrowLeft className="cursor-pointer" onClick={slideLeft} />
+          <FaArrowRight className="cursor-pointer" onClick={slideRight} />
         </p>
       </div>
-      <div className="ArtistProfile">
+      <div
+        ref={artistContainerRef}
+        className="ArtistProfile overflow-x-auto scrollNone transition-all ease-linear duration-200"
+      >
         {artistList.map((item, index) => {
-          
           return <Artistcard key={index} name={item.name} image={item.image} />;
         })}
       </div>
