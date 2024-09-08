0e6e3dfe98198ef4ec56c73addce047d5c38ced3
myselfpoulomi <routhpoulomi@gmail.com>
2024-09-08 21:56:56 +0530

    album row scroll

--- client/src/components/Album/AlbumRow.jsx
+++ client/src/components/Album/AlbumRow.jsx
@@ -1,8 +1,11 @@
-import React , { useEffect, useState } from 'react'
+import React , { useEffect, useRef, useState } from 'react'
 import axios from "axios";
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 
 function AlbumRow() {
   const [albumlist, setalbumlist] = useState([]);
+  const albumContainerRef = useRef(null);
   useEffect(() => {
     async function getAllAlbumbs() {
       try {
@@ -16,15 +19,43 @@ function AlbumRow() {
     }
     getAllAlbumbs();
   }, []);
+
+  function scrollRight() {
+    if (albumContainerRef.current) {
+      albumContainerRef.current.scrollTo({
+        left: albumContainerRef.current.scrollLeft + 300,
+        behavior: "smooth",
+      });
+    }
+  }
+  function scrollLeft() {
+    if (albumContainerRef.current) {
+      albumContainerRef.current.scrollTo({
+        left: albumContainerRef.current.scrollLeft - 300,
+        behavior: "smooth",
+      });
+    }
+  }
+
   return (
-    <div className='h-[400px] p-[1rem] px-[3rem]'>
-      <h2 className="text-white text-[35px] mb-9" >Albums</h2>
-      <div className="flex gap-[0.1rem] pl-[40px] mt-[20px] h-[78%]">
+    <div className='w-[100%] p-[20px]'>
+      <div className="flex text-white justify-between items-center mb-[40px]">
+      <h2 className="text-white text-4xl pl-[30px]" >Albums</h2>
+      <p className="flex gap-[20px] pr-[20px] text-lg">
+        <FaArrowLeft className="cursor-pointer" onClick={scrollLeft} />
+        <FaArrowRight className="cursor-pointer" onClick={scrollRight}  />
+      </p>
+      </div>
+      <div
+        ref={albumContainerRef}
+        className="overflow-x-auto scrollNone transition-all ease-linear duration-200"
+      >
+      <div className="flex gap-[0.1rem] pl-[40px] mt-[10px] h-[78%]">
         {albumlist.map((items, index) => {
           return (
-            <div className='h-[280px] w-[280px] hover:bg-[rgba(31,18,18,0.252)] rounded-[10px] flex flex-col items-center justify-center  transition-all duration-700 ease-in-out hover:shadow-[rgba(0,0,0,0.3)]'> 
+            <div className='h-[280px] min-w-[250px] hover:bg-[rgba(31,18,18,0.252)] rounded-[10px] flex flex-col items-center justify-center  transition-all duration-700 ease-in-out hover:shadow-[rgba(0,0,0,0.3)]'> 
               <img
-                className="h-[200px] w-[200px] object-cover rounded-[7px] mb-3 transform transition-transform duration-300 ease-in-out hover:scale-105 "
+                className="h-[210px] w-[210px] object-cover rounded-[7px] mb-3 transform transition-transform duration-300 ease-in-out hover:scale-105 "
                 src={items.image}
                 alt=""
               />
@@ -35,7 +66,7 @@ function AlbumRow() {
           );
         })}
       </div>
-      
+      </div>
     </div>
   )
 }
--- client/src/components/Songs/SongRow.jsx
+++ client/src/components/Songs/SongRow.jsx
@@ -22,7 +22,7 @@ function SongRow() {
     if (SongContainerRef.current) {
       SongContainerRef.current.scrollTo({
         left: SongContainerRef.current.scrollLeft + 300,
-        behavior: "smooth"
+        behavior: "smooth",
       });
     }
   }
@@ -30,40 +30,39 @@ function SongRow() {
     if (SongContainerRef.current) {
       SongContainerRef.current.scrollTo({
         left: SongContainerRef.current.scrollLeft - 300,
-        behavior: "smooth"
+        behavior: "smooth",
       });
     }
   }
 
-
   return (
     <div className=" w-[100%] p-[20px]">
       <div className="flex text-white justify-between items-center mb-[40px]">
-      <h2 className="text-white text-4xl pl-[30px]">Songs</h2>
-      <p className="flex gap-[20px] pr-[20px] text-lg">
-        <FaArrowLeft className="cursor-pointer" onClick={scrollLeft} />
-        <FaArrowRight className="cursor-pointer" onClick={scrollRight} />
-      </p>
+        <h2 className="text-white text-4xl pl-[30px]">Songs</h2>
+        <p className="flex gap-[20px] pr-[20px] text-lg">
+          <FaArrowLeft className="cursor-pointer" onClick={scrollLeft} />
+          <FaArrowRight className="cursor-pointer" onClick={scrollRight} />
+        </p>
       </div>
       <div
         ref={SongContainerRef}
         className="overflow-x-auto scrollNone transition-all ease-linear duration-200"
       >
-      <div className="flex gap-[0.1rem] pl-[40px] mt-[20px] h-[78%] ">
-        {songlist.map((items, index) => {
-          return ( 
-            <div className=" h-[280px] min-w-[250px] hover:bg-[rgba(31,18,18,0.252)] rounded-[10px] flex flex-col items-center justify-center  transition-all duration-700 ease-in-out hover:shadow-[rgba(0,0,0,0.3)]"> 
-              <img
-                className="h-[210px] w-[210px] object-cover rounded-[7px] mb-3 transform transition-transform duration-200 ease-in-out hover:scale-105"
-                src={items.image}
-                alt=""
-              /> 
-              <p className="text-white text-[20px] text-center mt-[5px]">
-                {items.title}
-              </p>
-            </div>
-          );
-        })}
+        <div className="flex gap-[0.1rem] pl-[40px] mt-[20px] h-[78%] ">
+          {songlist.map((items, index) => {
+            return (
+              <div className=" h-[280px] min-w-[250px] hover:bg-[rgba(31,18,18,0.252)] rounded-[10px] flex flex-col items-center justify-center  transition-all duration-700 ease-in-out hover:shadow-[rgba(0,0,0,0.3)]">
+                <img
+                  className="h-[210px] w-[210px] object-cover rounded-[7px] mb-3 transform transition-transform duration-200 ease-in-out hover:scale-105"
+                  src={items.image}
+                  alt=""
+                />
+                <p className="text-white text-[20px] text-center mt-[5px]">
+                  {items.title}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
