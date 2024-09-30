f98acc8f25659d1368f83876d12ed98ada2c3d52
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-30 19:37:02 +0530

    update UserController.js and UserRouter.js

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,30 +1,34 @@
-function sendOtp(req, res) {}
+// usercontroller.js
 
-function verifyOtpNCreateUser(req, res) {}
+/* ====== User Authentication ====== */
+function registerSendOtp(req, res) {}
+function registerVerify(req, res) {}
+function loginSendOtp(req, res) {}
+function loginVerifyOtp(req, res) {}
 
+/* ====== User Profile ====== */
 function updateName(req, res) {}
-
 function getUser(req, res) {}
 
+/* ====== Playlist Management ====== */
 function createPlaylist(req, res) {}
-
 function addSongToPlaylist(req, res) {}
-
 function removeSongFromPlaylist(req, res) {}
-
 function getAllPlaylists(req, res) {}
-
 function getPlaylist(req, res) {}
-
 function updatePlaylistName(req, res) {}
-
 function deletePlaylist(req, res) {}
 
 export {
-  sendOtp,
-  verifyOtpNCreateUser,
+  /* User Authentication */
+  registerSendOtp,
+  registerVerify,
+  loginSendOtp,
+  loginVerifyOtp,
+  /* User Profile */
   updateName,
   getUser,
+  /* Playlist Management */
   createPlaylist,
   addSongToPlaylist,
   removeSongFromPlaylist,
--- server/router/UserRouter.js
+++ server/router/UserRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {
-  sendOtp,
-  verifyOtpNCreateUser,
+  registerSendOtp,
+  registerVerify,
   updateName,
+  loginSendOtp,
+  loginVerifyOtp,
   getUser,
   createPlaylist,
   addSongToPlaylist,
@@ -15,17 +17,25 @@ import {
 
 const UserRouter = express.Router();
 
-UserRouter.post("/user/send-otp", sendOtp);
-UserRouter.post("/user/verify-otp/create-user", verifyOtpNCreateUser);
-UserRouter.put("/user/update-name/:userid", updateName);
-UserRouter.get("/user/get-user", getUser);
+/* ====== Registration Routes ====== */
+UserRouter.post("/user/register/send-otp", registerSendOtp);
+UserRouter.post("/user/register/verify-otp/create-user", registerVerify);
 
+/* ====== Login Routes ====== */
+UserRouter.post("/user/login/send-otp", loginSendOtp);
+UserRouter.post("/user/login/verify-otp", loginVerifyOtp);
+
+/* ====== User Profile Routes ====== */
+UserRouter.get("/user/get-user", getUser);
+UserRouter.put("/user/update-name/:userid", updateName);
+
+/* ====== Playlist Management Routes ====== */
 UserRouter.post("/user/create-playlist", createPlaylist);
 UserRouter.post("/user/add-song-to-playlist", addSongToPlaylist);
-UserRouter.post("/user/remove-song-to-playlist", removeSongFromPlaylist);
+UserRouter.post("/user/remove-song-from-playlist", removeSongFromPlaylist);
 UserRouter.get("/user/get-all-playlists/:userid", getAllPlaylists);
 UserRouter.get("/user/get-playlist/:playlistid", getPlaylist);
-UserRouter.put("/user/update-playlist-name/:playlistid", getPlaylist);
+UserRouter.put("/user/update-playlist-name/:playlistid", updatePlaylistName);
 UserRouter.delete("/user/delete-playlist/:playlistid", deletePlaylist);
 
 export default UserRouter;
